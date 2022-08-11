431013d12e891a07bdc655014643fff0da0431a4
Vlad <103497352+4epa@users.noreply.github.com>
2022-08-11 17:32:36 +0300

    Add progres bar

--- client/src/component/RaceHorseProces/RaceHorseProces.jsx
+++ client/src/component/RaceHorseProces/RaceHorseProces.jsx
@@ -1,11 +1,23 @@
-
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
 const RaceHorse = (props) => {
   return (
     <div className={props.distance === 1000 ? "horse__result_finish" : "horse__result" }>
-      <span>{props.name}</span>
-      <span>{props.distance}</span>
-      <span>m</span>
+      <div className='horse__container'>
+        <span>{props.name}</span>
+        <span>{props.distance}</span>
+        <span>m</span>
+      </div>
+      <Box sx={{ width: '100%' }}>
+        <Slider
+          size="small"
+          value={props.distance}
+          aria-label="Small"
+          valueLabelDisplay="auto"
+          max={1000}
+        />
+      </Box>
     </div>
   );
 };
